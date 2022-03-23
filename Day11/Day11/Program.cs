cce8b73f94007e0f57cec9d767e61d9b0d1d84e8
DoraLiv <dora.livkisa@gmail.com>
2022-03-24 00:24:00 +0200

    cities string search menu

--- Day11/Day11/Program.cs
+++ Day11/Day11/Program.cs
@@ -14,7 +14,7 @@ do
 Console.WriteLine($" The sum {sum} > 100, you have reached the end!");
 */
 
-
+/*
 Console.WriteLine("TASK 2: Number guessing game! Guess 3 numbers from 1 to 10");
 
 int[] answer = new int[3];
@@ -50,6 +50,65 @@ while (game)
             Console.WriteLine("You are wrong!");
             break;
     }
+    Console.WriteLine($"You have correctly guessed {guess[0]} {guess[1]} {guess[2]}");
+    bool isEqual = Enumerable.SequenceEqual(guess, answer);
+    if (isEqual == true)
+    {
+        game = false;
+        Console.WriteLine("Congrats! You won!");
+    }
+}
+*/
+Console.WriteLine("TASK 2: Number guessing game! Guess 3 numbers from 1 to 10");
+Random t = new Random();    
+int[] answer = new int[3];
+for (int i = 0; i < answer.Length; i++)
+{
+    int number;
+    do
+    {
+        number = t.Next(1, 10);
+    }while (answer.Contains(number));
+
+    answer[i] = number;
+}
+
+//Console.WriteLine($"The answer is {answer[0]} {answer[1]} {answer[2]}");
+
+int[] guess = new int[3];
+bool game = true;
+
+while (game)
+{//can be written outside the loop as well - then it will appear only at the beginning
+    Console.WriteLine("Enter a number 1-10:");
+    int.TryParse(Console.ReadLine(), out int numbers);
+    if (numbers == answer[0])
+    {
+        Console.WriteLine("You are correct!");
+        int guess1 = numbers;
+        guess[0] = guess1;
+    }
+    if (numbers == answer[1])
+    {
+        Console.WriteLine("You are correct!");
+        int guess1 = numbers;
+        guess[1] = guess1;
+    }
+    if (numbers == answer[2])
+    {
+        Console.WriteLine("You are correct!");
+        int guess1 = numbers;
+        guess[2] = guess1;
+    }
+    if (numbers != answer[0] && numbers != answer[1] && numbers != answer[2])
+    {
+        Console.WriteLine("You are wrong!");
+    }
+    if (numbers >10 || numbers <1)
+    {
+        Console.WriteLine("The number is out of range. Try again!");
+    }
+
     Console.WriteLine($"You have correctly guessed {guess[0]} {guess[1]} {guess[2]}");
     bool isEqual = Enumerable.SequenceEqual(guess, answer);
     if (isEqual == true)
---
+++ Day13-strings/Day13-strings/Program.cs
@@ -0,0 +1,215 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+/*
+char[] letters = { 'D', 'o', 'r','a' };
+string name = new string(letters);
+
+string equals = "Dora";
+*/
+
+using System.Text;
+using System.Text.RegularExpressions;
+
+string[] cities = { "Copenhagen", "Paris", "Tokyo", "Bristol", "Mumbai", "Dehli", "Riga", "Vienna", "Warsaw", "Hamburg", "Cesis" };
+
+StringBuilder starts = new StringBuilder();
+StringBuilder ends = new StringBuilder();
+StringBuilder lettercount = new StringBuilder();
+StringBuilder contains = new StringBuilder();
+//StringBuilder substringEN = new StringBuilder();
+
+
+int cCount = 0;
+bool menuOn=true;
+
+do
+{
+    Console.WriteLine("Please choose the search parameter:");
+    Console.WriteLine("1. Search by first letter");
+    Console.WriteLine("2. Search by last letter");
+    Console.WriteLine("3. search by the number of letters");
+    Console.WriteLine("4. search by partial match");
+    Console.WriteLine("* quit search\n");
+    string choise = Console.ReadLine();
+    Console.WriteLine();
+    switch (choise)
+    {
+        case "1":
+
+            bool menufirst = true;
+            do
+            {
+                Console.WriteLine("Please insert the first letter:");
+                Console.WriteLine("Type * to return to main menu");
+                Console.WriteLine();
+                string firstletter = Console.ReadLine();
+                Console.WriteLine();
+
+                if (firstletter != "*")
+                {
+                    char first = firstletter [0];
+                    Regex re = new Regex($"^{first}", RegexOptions.IgnoreCase);
+                    int j = 1;
+                    cCount = 0;
+                    for (int i = 0; i < cities.Length; i++)
+                        if (re.IsMatch(cities[i]))
+                        {
+                            cCount++;           
+                            starts.Append($"{j}.{cities[i]}; ");
+                            j++;
+                        }
+                    Console.WriteLine($"There are {cCount} cities starting with \"{first}\":");
+                    Console.WriteLine(starts); 
+                    Console.WriteLine();
+                    starts = new StringBuilder();
+                }
+                else if (firstletter == "*")
+                {
+                    menufirst = false;
+                }
+                
+            } while (menufirst);
+
+            break;
+
+        case "2":
+            bool menulast = true;
+            do
+            {
+                Console.WriteLine("Please insert the last letter:");
+                Console.WriteLine("Type * to return to main menu");
+                Console.WriteLine();
+                string lastletter = Console.ReadLine();
+                Console.WriteLine();
+
+                if (lastletter != "*")
+                {
+                    char last = lastletter[0];
+                    Regex re2 = new Regex($"{last}$", RegexOptions.IgnoreCase);
+                    int j = 1;
+                    cCount = 0;
+                    for (int i = 0; i < cities.Length; i++)
+                       if (re2.IsMatch(cities[i]))
+                       {
+                            cCount++;
+                            ends.Append($"{j}.{cities[i]}; ");
+                            j++;
+                       }
+                    Console.WriteLine($"There are {cCount} cities ending with \"{last}\":");
+                    Console.WriteLine(ends);
+                    Console.WriteLine();
+                    ends = new StringBuilder();
+                    
+                }
+                else if (lastletter == "*")
+                {
+                    menulast = false;
+                }
+                
+            } while (menulast);
+            break;
+
+        case "3":
+            bool menucount = true;
+            do
+            {
+                Console.WriteLine("Enter the number of letters:");
+                Console.WriteLine("Type * to return to main menu");
+                Console.WriteLine();
+                string letters = Console.ReadLine();
+                Console.WriteLine();
+
+                if (letters != "*")
+                {
+                    Regex check = new Regex(@"^\d+$");
+                    if (check.IsMatch(letters))
+                    {
+                        int.TryParse((letters), out int number);
+                        Regex re3 = new Regex($"^[a-zA-Z]{{{number}}}$");
+                        int j = 1;
+                        cCount = 0;
+                        for (int i = 0; i < cities.Length; i++)
+                            if (re3.IsMatch(cities[i]))
+                            {
+                                cCount++;
+                                lettercount.Append($"{j}.{cities[i]}; ");
+                                j++;
+                            }
+                        Console.WriteLine($"There are {cCount} cities containing exactly {letters} letters:");
+                        Console.WriteLine(lettercount);
+                        Console.WriteLine();
+                        lettercount = new StringBuilder();
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid input value. Try again!");
+                        Console.WriteLine();
+                    }
+                    
+
+                }
+                else if (letters == "*")
+                {
+                    menucount = false;
+                }
+
+            } while (menucount);
+            break;
+
+        case "4":
+            bool menuPart = true;
+            do
+            {
+                Console.WriteLine("Enter part of the word:");
+                Console.WriteLine("Type * to return to main menu");
+                Console.WriteLine();
+                string part = Console.ReadLine();
+                Console.WriteLine();
+
+                if (part != "*")
+                {
+                    Regex re4 = new Regex($"^.*{part}.*$", RegexOptions.IgnoreCase);
+                    int j = 1;
+                    cCount = 0;
+
+                    for (int i = 0; i < cities.Length; i++)
+
+                        if (re4.IsMatch(cities[i]))
+                        {
+                            cCount++;
+                            contains.Append($"{j}.{cities[i]}; ");
+                            j++;
+
+                        }
+                    Console.WriteLine($"There are {cCount} cities containing \"{part}\":");
+                    Console.WriteLine(contains);
+                    Console.WriteLine();
+                    contains = new StringBuilder();
+                }
+                else if (part == "*")
+                {
+                    menuPart = false;
+                }
+            } while (menuPart);
+            break;
+
+        case "*":
+            Console.WriteLine("Bye!");
+            menuOn=false;
+            break;
+
+        default:
+            Console.WriteLine("Invalid choice. Try again!");
+            Console.WriteLine();
+            break;
+
+
+    }
+} while (menuOn);
+
+
+
+
+
+
+
