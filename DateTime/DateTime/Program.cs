f0472b4ba930303dc0bfb7f1065e81c0ade4740c
DoraLiv <dora.livkisa@gmail.com>
2022-03-27 23:07:18 +0300

    datetime count survey

---
+++ DateTime/DateTime/Program.cs
@@ -0,0 +1,68 @@
+﻿// See https://aka.ms/new-console-template for more information
+DateTime start = DateTime.Now;
+int count1 = 0; //number of times option1 is picked
+int count2 = 0; //number of times option2 is picked
+int count3 = 0; //number of times option3 is picked
+int countall = 0; //total number of answers 
+
+bool survey = true;
+do
+{
+    Console.WriteLine("Enter your birthday:");
+    Console.WriteLine("Date: ");
+    string day = Console.ReadLine();
+    Console.WriteLine("Month: ");
+    string month = Console.ReadLine();
+    Console.WriteLine("Year: ");
+    string year = Console.ReadLine();
+    string bday = $"{year}-{month}-{day}";
+    //bday.ToString(yyyy-mm-dd);
+
+    Console.WriteLine($"Your birthday is {bday}");
+
+    Console.WriteLine("Choose an option:");
+    Console.WriteLine("1 - option1");
+    Console.WriteLine("2 - option2");
+    Console.WriteLine("3 - option3");
+    Console.WriteLine("* - exit app");
+    char.TryParse(Console.ReadLine(), out char choice);
+
+    if (choice == '*')
+    {
+        survey = false;
+    }
+    else if (choice != '*')
+    {
+        countall++;
+        switch (choice)
+        {
+            case '1':
+                Console.WriteLine("You chose option 1.");
+                count1++;
+                
+                break;
+
+            case '2':
+                Console.WriteLine("You chose option 2.");
+                count2++;
+                break;
+
+            case '3':
+                Console.WriteLine("You chose option 3.");
+                count3++;
+                break;
+
+            default:
+                break;
+
+        }
+    }
+
+} while (survey);
+DateTime stop = DateTime.Now;
+Console.WriteLine($"Total number of answers: {countall}");
+Console.WriteLine($"Number of option 1 votes: {count1}");
+Console.WriteLine($"Number of option 1 votes: {count2}");
+Console.WriteLine($"Number of option 1 votes: {count3}");
+Console.WriteLine($"Time survey started: {start}");
+Console.WriteLine($"Time survey finished: {stop}");
---
+++ Day15class-cardsPalindromes/Day15class-cardsPalindromes/Program.cs
@@ -0,0 +1,93 @@
+﻿// See https://aka.ms/new-console-template for more information
+/*
+using System.Text.RegularExpressions;
+
+Console.WriteLine($"===Palindromes===");
+
+Console.WriteLine($"Write your sentence:");
+string sentence1 = Console.ReadLine().ToLower();
+Regex nospaces = new Regex("[a-zA-Z]");
+string sentence1nospace = string.Empty;   
+for (int i = 0; i < sentence1.Length; i++)
+{
+    if (nospaces.IsMatch(sentence1[i].ToString()))
+    {
+        sentence1nospace += sentence1[i];
+
+    }
+}
+string sentence1reversed = string.Empty;
+bool ispalindrome;
+for (int i = sentence1nospace.Length-1; i >=0 ; i--)
+{
+    sentence1reversed += sentence1nospace[i];
+}
+
+ispalindrome = sentence1nospace.Equals(sentence1reversed);
+Console.WriteLine(sentence1reversed);
+Console.WriteLine($"You have entered a palindrome: {ispalindrome}");
+*/
+
+
+/*
+Console.WriteLine($"===Cards===");
+
+    public enum Suits
+    {
+        Spades,
+        Clubs,
+        Hearts,
+        Diamonds
+    }
+    public enum CardValues
+    {
+        Two,
+        Three,
+        Four,
+        Five,
+        Six,
+        Seven,
+        Eight,
+        Nine,
+        Ten,
+        Jack,
+        Queen,
+        King,
+        Ace
+    }
+    public class cards
+    {
+        static public void Shuffle(string[] cardPack)
+        {
+            Random rnd = new Random();
+            for (int i = cardPack.Length-1; i>0; --i)
+            {
+                int k = rnd.Next(i+1);
+                string temp = cardPack[i];
+                cardPack[i] = cardPack[k];
+                cardPack[k] = temp;
+            }
+
+        }
+        static void Main(string[] args)
+        {
+            string[] cardPack = new string[52];
+            int position = 0;
+            foreach (string suit in Enum.GetNames(typeof(Suits)))
+            {
+                foreach (string values in Enum.GetNames(typeof(CardValues)))
+                {
+                    cardPack[position] = $"{values} of {suit}";
+                    position++;
+                }
+            }
+            Console.WriteLine($"The first card is {cardPack[0]}");
+            Shuffle(cardPack);
+            for (int i = 0; i <6; i++)    //getting random 6 cards
+            {
+                Console.Write($"{cardPack[i]}");
+            }
+        }
+    }
+*/
+
